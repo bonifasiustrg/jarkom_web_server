88b96520bdab37bf7ab47e89f0bd68342c9ac50a
Bonifasius Tarigan <bonifasius.tambak@gmail.com>
2023-05-07 20:58:53 +0700

    add html response, and not found handler

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 #import socket module
 from socket import *
+import os # In order to get the current working directory
 import sys # In order to terminate the program
 
 #Prepare a sever socket
@@ -11,13 +12,93 @@ serverSocket.bind(('', serverPort))
 serverSocket.listen(1)
 print('The server is ready to receive')
 
-#while yanh templete
+
+#parsing http request
 while True:
-    connectionSocket, addr = serverSocket.accept()
-    sentence = connectionSocket.recv(1024).decode()
-    capitalizedSentence = sentence.upper()
-    connectionSocket.send(capitalizedSentence.encode())
-    connectionSocket.close()
+        # Wait for client connections
+    client_connection, client_address = serverSocket.accept()
+
+    # Get the client request
+    request = client_connection.recv(1024).decode()
+    print(request)
+
+
+        # Parse HTTP headers
+    headers = request.split('\n')
+    filename = headers[0].split()[1]
+
+    # Get the content of the file
+    if filename == '/':
+        filename = '/index.html'
+
+    try:
+        # fin = open('htdocs' + filename)
+        # content = fin.read()
+        with open('htdocs' + filename, 'rb') as fin:
+            content = fin.read().decode('utf-8', 'ignore')
+        
+        fin.close()
+
+        response = 'HTTP/1.0 200 OK\n\n' + content
+    except FileNotFoundError:
+
+        response = 'HTTP/1.0 404 NOT FOUND\n\nFile Not Found'
+
+    # Send HTTP response
+    client_connection.sendall(response.encode())
+    
+    # # Send HTTP response
+    # response = 'HTTP/1.0 200 OK\n\nHello World'
+    # client_connection.sendall(response.encode())
+    # client_connection.close()
+
+    
+    """ # Terima koneksi dari klien
+    client_connection, client_address = serverSocket.accept()
+    
+    # Terima HTTP request dari klien
+    request = client_connection.recv(1024)
+    
+    try:
+        # Parse HTTP request
+        request_parts = request.split(b'\r\n')
+        request_line = request_parts[0].decode('utf-8')
+        method, path, version = request_line.split()
+        
+        # Cari file yang diminta oleh klien
+        file_path = os.getcwd() + path
+        
+        
+        if os.path.isfile(file_path):
+            # Jika file tersedia, baca isinya
+            with open(file_path, 'rb') as file:
+                content = file.read()
+            
+            # Buat HTTP response message
+            response_line = b'HTTP/1.1 200 OK\r\n'
+            response_header = b'Content-Type: text/html\r\n'
+            response_body = content
+        else:
+            # Jika file tidak tersedia, kirim pesan 404 Not Found
+            response_line = b'HTTP/1.1 404 Not Found\r\n'
+            response_header = b'Content-Type: text/plain\r\n'
+            response_body = b'404 Not Found'
+        
+        # Kirim HTTP response message ke klien
+        response = response_line + response_header + b'\r\n' + response_body
+        client_connection.sendall(response)
+    
+    except ValueError:
+        # Jika permintaan HTTP tidak memiliki format yang benar, kirim pesan 400 Bad Request
+        response_line = b'HTTP/1.1 400 Bad Request\r\n'
+        response_header = b'Content-Type: text/plain\r\n'
+        response_body = b'400 Bad Request'
+        response = response_line + response_header + b'\r\n' + response_body
+        client_connection.sendall(response) """
+    
+    # Tutup koneksi dengan klien
+    client_connection.close()
+
 
 #while yang dikerjakan
 """ #Fill in end
